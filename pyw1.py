6f613e25d45bc399295bffcf050c2c20a42df578
mamidiakshith <151131099+mamidiakshith@users.noreply.github.com>
2024-09-25 19:25:04 +0530

    Add files via upload

---
+++ pyw1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+
+
+# In[2]:
+
+
+a=int(input("enter a number "))
+b=int(input("enter a number "))
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+
+# In[4]:
+
+
+b=90
+if b!=90 :
+    print(kL)
+
+
+# In[8]:
+
+
+import math
+p=1000
+t=12
+r=6
+c=(p*(1+(r/100)**t))-p
+print(c)
+
+
+# In[11]:
+
+
+import math
+x1=int(input("enter coordinate"))
+x2=int(input("enter coordinate"))
+y1=int(input("enter coordinate"))
+y2=int(input("enter coordinate"))
+d=(x2-x1)**2 +(y2-y1)**2
+f=d**(1/2)
+print(f)
+
+
+# In[16]:
+
+
+name=input("enter your name")
+address=input("enter address")
+phone=int(input("enter number"))
+print("name:" + name)
+print("address:"+ address)
+print("mobile no:", phone)
+
+
+# In[ ]:
+
+
+
+
